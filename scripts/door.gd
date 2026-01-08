cfc70e6a085e826b108a555a2aa6d976ef235573
PedroToma <pedroakira.toma@gmail.com>
2026-01-07 21:59:08 -0300

    Criação de sistema de porta e alavanca.

--- scripts/door.gd
+++ scripts/door.gd
@@ -51,7 +51,6 @@ func _on_body_entered(_body: Node2D) -> void:
 func _on_body_exited(_body: Node2D) -> void:
 	can_interact = false
 
-
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if anim.animation == "opening":
 		status = DoorState.opened
