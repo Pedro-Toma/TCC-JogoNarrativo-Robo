37f4cfaf9bd004e62b5c18395908d855e76c1100
PedroToma <pedroakira.toma@gmail.com>
2026-01-25 02:30:59 -0300

    Teste com audio ao capturar item (luva)

--- scripts/gloves.gd
+++ scripts/gloves.gd
@@ -1,9 +1,20 @@
 extends Area2D
 
+@onready var audio: AudioStreamPlayer = $AudioStreamPlayer
+
+var is_collected = false
+
 func _on_body_entered(body: Node2D) -> void:
+	if is_collected:
+		return
+		
 	if body.is_in_group("Player"):
-		print("ENTROU")
+		is_collected = true
 		if body.has_method("enable_wall_jump"):
 			body.enable_wall_jump()
 			GameState.has_wall_jump = true
+			audio.play()
+			hide()
+			set_deferred("monitoring", false)
+			await audio.finished 
 			queue_free()
